a234e889336c983f065fa53a391cbd786a6ac1be
Levi Kane <oneleggedlevi09@gmail.com>
2021-05-21 15:27:20 -0500

    model is set up right. still need to add it's aggregate function

--- models/Workout.js
+++ models/Workout.js
@@ -1,14 +1,45 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const exercises = {
+  type: {
+    type: String,
+    required: 'please choose cardio or resistance',
+    enum: ['cardio', 'resistance']
+  },
+  name: {
+    type: String,
+    required: 'name your workout'
+  },
+  duration: {
+    type: Number,
+    required: 'Please enter a duration'
+  },
+  weight: {
+    type: Number
+  },
+  reps: {
+    type: Number
+  },
+  sets: {
+    type: Number
+  },
+  distance: {
+    type: Number
+  }
+};
+
 const WorkoutSchema = new Schema({
-    name: {
-        type: String,
-        unique: true
-      },
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercises: [exercises]
 });
 
-const Workout = mongoose.model("Workout", WorkoutSchema);
+WorkoutSchema;
+
+const Workout = mongoose.model('Workout', WorkoutSchema);
 
 module.exports = Workout;

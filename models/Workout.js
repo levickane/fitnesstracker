2783df7e2a7ac68aca3c937ccefac3d39af02e6e
Levi Kane <oneleggedlevi09@gmail.com>
2021-05-21 23:12:35 -0500

    the pages are rendering but for some reason the data is not getting created in the database.

--- models/Workout.js
+++ models/Workout.js
@@ -2,39 +2,66 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const exerciseData = {
-  type: {
-    type: String,
-    required: 'please choose cardio or resistance'
-  },
-  name: {
-    type: String,
-    required: 'name your workout'
-  },
-  duration: {
-    type: Number,
-    required: 'Please enter a duration'
-  },
-  weight: {
-    type: Number
-  },
-  reps: {
-    type: Number
-  },
-  sets: {
-    type: Number
-  },
-  distance: {
-    type: Number
-  }
-};
+// const exerciseData = {
+//   type: {
+//     type: String,
+//     required: 'please choose cardio or resistance'
+//   },
+//   name: {
+//     type: String,
+//     required: 'name your workout'
+//   },
+//   duration: {
+//     type: Number,
+//     required: 'Please enter a duration'
+//   },
+//   weight: {
+//     type: Number
+//   },
+//   reps: {
+//     type: Number
+//   },
+//   sets: {
+//     type: Number
+//   },
+//   distance: {
+//     type: Number
+//   }
+// };
 
 const WorkoutSchema = new Schema({
   day: {
     type: Date,
     default: Date.now
   },
-  exercises: [exerciseData]
+  exercises: [
+    {
+      type: {
+        type: String,
+        required: 'please choose cardio or resistance'
+      },
+      name: {
+        type: String,
+        required: 'name your workout'
+      },
+      duration: {
+        type: Number,
+        required: 'Please enter a duration'
+      },
+      weight: {
+        type: Number
+      },
+      reps: {
+        type: Number
+      },
+      sets: {
+        type: Number
+      },
+      distance: {
+        type: Number
+      }
+    }
+  ]
 });
 
 //this dynamically creates a property in the schema and then
--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -3,18 +3,20 @@ const Workout = require('../models/Workout');
 
 //this gets our last workout
 router.get('/api/workouts', (req, res) => {
-  console.log('workout rout hit**********');
-  Workout.find()
+  console.log('***HOME PAGE***');
+  Workout.find({})
     .then((workoutDB) => {
+      console.log('hello', workoutDB);
       res.json(workoutDB);
     })
     .catch((err) => {
-      res.status(500).json(err);
+      res.status(400).json(err);
     });
 });
 
 //this gets all workouts
 router.get('/api/workouts/range', (req, res) => {
+  console.log('***STATS PAGE****');
   Workout.find({})
     .limit(7)
     .sort({ date: -1 })
@@ -22,23 +24,24 @@ router.get('/api/workouts/range', (req, res) => {
       res.json(workoutDB);
     })
     .catch((err) => {
-      res.status(500).json(err);
+      res.status(400).json(err);
     });
 });
 
 router.post('/api/workouts', ({ body }, res) => {
-  console.log('****CREAT WORKOUT*****');
+  console.log('****CREAT WORKOUT ROUTE*****');
   Workout.create(body)
     .then((workoutDB) => {
+      console.log('Hello', workoutDB);
       res.json(workoutDB);
     })
     .catch((err) => {
-      res.status(500).json(err);
+      res.status(400).json(err);
     });
 });
 
-router.post('/api/workouts/:id', ({ body, params }, res) => {
-  console.log(body);
+router.put('/api/workouts/:id', ({ body, params }, res) => {
+  console.log('updating working stuff', body);
   Workout.findByIdAndUpdate(
     params.id,
     { $push: { exercises: body } },
@@ -48,7 +51,7 @@ router.post('/api/workouts/:id', ({ body, params }, res) => {
       res.json(workoutDB);
     })
     .catch((err) => {
-      res.status(500).json(err);
+      res.status(400).json(err);
     });
 });
 
